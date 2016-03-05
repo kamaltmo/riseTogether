44af0c048b3181e5ccde08a4c5c822387323114c
Kamal Osman <kamaltmo@gmail.com>
2016-03-05 11:19:18 +0000

    Final Database

--- risetogether.sql
+++ risetogether.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2016 at 02:09 AM
+-- Generation Time: Mar 05, 2016 at 12:18 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -39,7 +39,81 @@ CREATE TABLE IF NOT EXISTS `profile` (
   `facebook` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `linkedin` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `xp` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`ID`, `firstName`, `lastName`, `email`, `password`, `pic`, `website`, `cproject`, `bio`, `facebook`, `linkedin`, `xp`) VALUES
+(1, 'test', 'test', 'g@g.com', 'gg', 'ghghghghhg', '', '', 'This is a test of the bio feild ', '', '', 20),
+(2, '', '', 'g@gg.com', '', '', '', '', '', '', '', 0),
+(3, '', '', 'test@gmail.com', 'kkkkkkkkk', '', '', '', '', '', '', 100),
+(4, '', '', 'g@dg.com', '149616', '', '', '', '', '', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profilerequests`
+--
+
+CREATE TABLE IF NOT EXISTS `profilerequests` (
+  `senderID` int(11) NOT NULL,
+  `receiverID` int(11) NOT NULL,
+  `requestID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profileskills`
+--
+
+CREATE TABLE IF NOT EXISTS `profileskills` (
+  `profileID` int(255) NOT NULL,
+  `skillsID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profileskills`
+--
+
+INSERT INTO `profileskills` (`profileID`, `skillsID`) VALUES
+(1, 2),
+(1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE IF NOT EXISTS `requests` (
+  `ID` int(255) NOT NULL,
+  `title` varchar(50) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  `info` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+CREATE TABLE IF NOT EXISTS `skills` (
+  `ID` int(11) NOT NULL,
+  `skillName` varchar(50) NOT NULL,
+  `skillInfo` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `skills`
+--
+
+INSERT INTO `skills` (`ID`, `skillName`, `skillInfo`) VALUES
+(2, 'DAAADA', 'DAS BOOT OO YEA'),
+(4, 'r', 'rrr');
 
 --
 -- Indexes for dumped tables
@@ -52,6 +126,18 @@ ALTER TABLE `profile`
   ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `password` (`password`);
 
 --
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -59,7 +145,17 @@ ALTER TABLE `profile`
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `requests`
+--
+ALTER TABLE `requests`
   MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `skills`
+--
+ALTER TABLE `skills`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
